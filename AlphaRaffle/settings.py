5c73fa70a479ce44c438390490bd049460c70253
dbswlrla112@naver.com <dbswlrla112@naver.com>
2022-08-22 16:55:57 +0900

    3123214

--- AlphaRaffle/settings.py
+++ AlphaRaffle/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_heroku
+django_heroku.settings(locals())
