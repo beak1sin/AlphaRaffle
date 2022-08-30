200f296fe8a707bc24eafd44f3e84f33746b40bb
dbswlrla112@naver.com <dbswlrla112@naver.com>
2022-08-30 16:15:13 +0900

    202208301615

--- AlphaRaffle/settings.py
+++ AlphaRaffle/settings.py
@@ -27,8 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = my_settings.SECRET
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-j_st-lkoalyake(0_tr^uw=jc&oqpm0!+dn9##j&8b_^12ua2z')
 
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -100,9 +98,11 @@ DATABASES = {
       'PASSWORD': 'lp62xkq1xtc0vwvn',
       'HOST': 'r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
       'PORT': '3306',
+      'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
   }
 }
-
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
@@ -149,3 +149,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
