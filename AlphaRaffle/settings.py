b46c6ce03007aee7b66b2cedf66dc91a1908003a
dbswlrla112@naver.com <dbswlrla112@naver.com>
2022-09-06 13:12:06 +0900

    202209061312

--- AlphaRaffle/settings.py
+++ AlphaRaffle/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['https://alpharaffle.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['alpharaffle.herokuapp.com','127.0.0.1']
 
 
 # Application definition
