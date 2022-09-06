18d2628ae6096f0ca5d214e036fbed6553bc1daf
dbswlrla112@naver.com <dbswlrla112@naver.com>
2022-09-06 13:52:07 +0900

    202209061352

--- AlphaRaffle/settings.py
+++ AlphaRaffle/settings.py
@@ -90,22 +90,22 @@ DATABASES = {
 }
 """
 
-# DATABASES = {
-#   'default' : {
-#       'ENGINE': 'django.db.backends.mysql',
-#       'NAME': 'z5qqc4mh1ih5pdyw',
-#       # 'NAME': 'AlphaRaffle',
-#       'USER': 'c4ddvl7d29vbhdbw', #주로 'root'
-#       'PASSWORD': 'lp62xkq1xtc0vwvn',
-#       'HOST': 'r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-#       'PORT': '3306',
-#       'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         },
-#   }
-# }
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+DATABASES = {
+  'default' : {
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': 'z5qqc4mh1ih5pdyw',
+      # 'NAME': 'AlphaRaffle',
+      'USER': 'c4ddvl7d29vbhdbw', #주로 'root'
+      'PASSWORD': 'lp62xkq1xtc0vwvn',
+      'HOST': 'r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      'PORT': '3306',
+      'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+  }
+}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
