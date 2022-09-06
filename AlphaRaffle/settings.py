8f9c32e93bc6a56b1ad4111eb0654268867da125
dbswlrla112@naver.com <dbswlrla112@naver.com>
2022-09-06 15:21:13 +0900

    202209061521

--- AlphaRaffle/settings.py
+++ AlphaRaffle/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os, environ
+import os
+import environ
 from pathlib import Path
 # import my_settings
 import dj_database_url
