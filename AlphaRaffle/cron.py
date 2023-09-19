f6168ad3dc8c2a51e4c0add76ba76b24699b3d0f
dbswlrla112@naver.com <dbswlrla112@naver.com>
2023-09-19 19:11:43 +0900

    202309191911

--- AlphaRaffle/cron.py
+++ AlphaRaffle/cron.py
@@ -93,8 +93,7 @@ def luckd_crowler(no):
     time.sleep(0.01)
     #신발 한글이름
     shoename = soup.select("h1.page_title")[0].text.replace('/', '_')
-    shoename = shoename.replace('%', '♪')
-    shoename = shoename.replace('+', '♣')
+    
     #신발 영어이름
     try:
         subname  = soup.select("h2.sub_title")[0].text
