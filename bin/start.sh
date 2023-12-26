5fe78d91236a630491e54b50e9750b6bd32a3f89
dbswlrla112@naver.com <dbswlrla112@naver.com>
2023-12-26 14:47:45 +0900

    202312261447

--- bin/start.sh
+++ bin/start.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 . /root/.virtualenvs/AlphaRaffle_env/bin/activate
 echo "############# virtualenvs ON #############"
-python /usr/local/share/AlphaRaffle/manage.py makemigrations
+python /usr/local/share/docker-server/AlphaRaffle/manage.py makemigrations
 echo "############# makemigrations OK #############"
-python /usr/local/share/AlphaRaffle/manage.py migrate
+python /usr/local/share/docker-server/AlphaRaffle/manage.py migrate
 echo "############# migrate OK #############"
-python /usr/local/share/AlphaRaffle/manage.py collectstatic --noinput
+python /usr/local/share/docker-server/AlphaRaffle/manage.py collectstatic --noinput
 echo "############# collectstatic OK #############"
-killall -9 uwsgi
-echo "############# KILL OK #############"
-uwsgi --ini /usr/local/share/AlphaRaffle/AlphaRaffle_uwsgi.ini &
-echo "############# START OK (BackGround) ############"
+# killall -9 uwsgi
+# echo "############# KILL OK #############"
+# uwsgi --ini /usr/local/share/AlphaRaffle/AlphaRaffle_uwsgi.ini &
+# echo "############# START OK (BackGround) ############"
 deactivate
 echo "############# virtualenvs OFF #############"
 #tail -111f /usr/local/share/AlphaRaffle/logs/uwsgi.log
